a061c39d1605f53789e55bdabf4e6b505dfea886
xKang <xkangxyz@gmail.com>
2019-09-08 15:33:47 +1200

    2019年9月8日14:01:18 improve program

--- program/main/main.ino
+++ program/main/main.ino
@@ -6,18 +6,16 @@
 #include <DS1307RTC.h>
 
 #define INTERPUT_PIN 2 //over current interput
-#define LDR_PIN 5
-#define REED_PIN 8
-#define BUTTON_PIN 9
+#define LDR_PIN A1
+#define REED_PIN 7
+#define HES_PIN A2
+#define BUTTON_PIN A3
 
 #define TIME_TO_OPEN 13
 #define LIGHT_TO_OPEN 500
 
 #define TIME_TO_CLOSE 13
 #define LIGHT_TO_CLOSE 500
-
-tmElements_t tm;
-
 
 enum status { //waiting for...
   timeOpen,
@@ -30,21 +28,25 @@ enum status { //waiting for...
   eStop
 };
 
-L298N motor(11, 12, 13);
-LiquidCrystal LCD(9, 10, 4, 5, 6, 7);
-//L298N motor(EN, IN1, IN2);
-//LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
+L298N _motor(9, 10, 8);
+LiquidCrystal _lcd(5, 6, 11, 12, 13, A0);
+//L298N _motor(EN, IN1, IN2);
+//LiquidCrystal _lcd(rs, en, d4, d5, d6, d7);
+
 status _status = timeOpen;
-char _LCDText[2][16];
+String _lcdText[2];
+tmElements_t tm;
 
 void setup() {
   //interput...
-  pinMode(INTERPUT_PIN, INPUT); 
+  pinMode(INTERPUT_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(INTERPUT_PIN), overHeatAction, LOW);
-  
+
   pinMode(LDR_PIN, INPUT);
   pinMode(REED_PIN, INPUT);
 
+  _lcd.begin(16, 2);
+  _lcd.autoscroll();
 }
 
 void loop() {
@@ -74,8 +76,6 @@ void loop() {
     }
   }
   //read button
-  //update screen
-
 }
 
 void timeOpenFun(){
@@ -84,7 +84,7 @@ void timeOpenFun(){
       _status = lightOpen;
     }else{
       setText("waiting timer", 0);
-      setText("manual open", 1);
+      setText("manually open", 1);
     }
   }
 }
@@ -94,29 +94,27 @@ void lightOpenFun(){
     _status = opening;
   }else{
     setText("waiting light", 0);
-    setText("manual open", 1);
+    setText("manually open", 1);
   }
 }
 void openingFun(){ //it would not jump back to main switch, so I have to deal with button press
   setText("opening", 0);
   setText("emergency stop", 1);
-  motor.setSpeed(225);
-  motor.forward();
+  _motor.setSpeed(225);
+  _motor.forward();
   while(digitalRead(REED_PIN) == HIGH){
     if(checkEStop()){
-      _status = eStop;
       return;
     }
   }
   while(digitalRead(REED_PIN) ==  LOW){
     if(checkEStop()){
-      _status = eStop;
       return;
     }
   }
-  motor.fastStop();
+  _motor.fastStop();
   delay(500);
-  motor.disable();
+  _motor.disable();
   _status = lightClose;
 
 }
@@ -129,10 +127,6 @@ void lightCloseFun(){
 void closeingFun(){
 
 }
-void overHeatAction(){
-  motor.disable();
-  _status = overheat;
-}
 void overheatFun(){
   setText("over heat", 0);
   setText("pls clear jam and reset", 1);
@@ -142,17 +136,34 @@ void eStopFun(){
   //just waiting for continue
 }
 
+
+void overHeatAction(){ //for interput
+  _motor.disable();
+  _status = overheat;
+}
+
 bool checkEStop(){
-  if(BUTTON_PIN > 0 && BUTTON_PIN < 200){
-    motor.fastStop();
+  return false; //!!error!!
+  if(BUTTON_PIN > 0 && BUTTON_PIN < 200){ //!!error!!
+    _motor.fastStop();
+    _status = eStop;
     delay(500);
-    motor.disable();
+    _motor.disable();
     setText("emergency stop", 0);
     setText("continue", 1);
     return true;
   }
   return false;
 }
-void setText(String abc, int line){
-  //strncpy(*_LCDText[line], text, sizeof(_LCDText[line]));
+
+void setText(String text, int line){
+  if (text.equals(_lcdText[line])){
+    return;
+  }
+  if(line == 0){
+    _lcd.clear();
+  }else{
+    _lcd.setCursor(0,line);
+  }
+  _lcd.print(text);
 }
